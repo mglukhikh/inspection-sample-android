088d30f169060b83fa3d5205bfc44143176a01eb
Mikhail Glukhikh <glukhikh@mail.ru>
2018-08-28 17:55:55 +0300

    Initial commit (android inspection plugin sample)

---
+++ app/src/main/java/com/example/oucouppnique/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.oucouppnique.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+public val x: Int = 0
